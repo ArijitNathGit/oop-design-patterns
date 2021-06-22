2d65658303e611cd7fed002694c7efabea839876
ArijitNathGit <arijitnath18@gmail.com>
2021-06-22 21:13:33 +0530

    AdapterDesignPattern

--- src/com/arijit/designpattern/structural/adapter/AdapterDesignPattern.java
+++ src/com/arijit/designpattern/structural/adapter/AdapterDesignPattern.java
@@ -1,5 +1,17 @@
 package com.arijit.designpattern.structural.adapter;
 
+/**
+ * Advantages:
+
+ * Helps achieve reusability and flexibility.
+ * Client class is not complicated by having to use a different interface and can use polymorphism to swap between different implementations of adapters.
+ * 
+ * Disadvantages:
+ *
+ * All requests are forwarded, so there is a slight increase in the overhead.
+ * Sometimes many adaptations are required along an adapter chain to reach the type which is required.
+ * 
+ * */
 public class AdapterDesignPattern {
 
 	public static void main(String[] args) {
